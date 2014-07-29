ad12e79631b2b12556ac3e506151e05bde036d49
root <root@KPTest.(none)>
2014-07-29 20:52:59 +0800

    upload epoll_test_ser.cpp

---
+++ epoll_test_ser.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<stdlib.h>
+#include<signal.h>
+#include<sys/wait.h>
+#include<sys/epoll.h>
+#include<fcntl.h>
+using namespace std;
+
+int Socket(int af,int type,int protocol){
+	int fd =socket(af,type,protocol);
+	if(fd < 0){
+		cout<<"error"<<endl;
+		exit(-1);
+	}
+	return fd;
+}
+
+int Bind(int fd,struct sockaddr *addr,int len){
+	int ret = bind(fd,addr,len);
+	if(ret < 0){
+		cout<<"error"<<endl;
+		exit(-1);
+	}
+	return ret;
+}
+
+int Listen(int fd,int size){
+	int ret = listen(fd,1024);
+	if(ret < 0){
+		cout<<"error"<<endl;
+		exit(-1);
+	}
+	return ret;
+}
+
+int main(int argc,char **argv){
+	int ser_fd,ep_fd;
+	struct sockaddr_in ser_addr;
+	
+	bzero(&ser_addr,sizeof(ser_addr));
+	ser_addr.sin_family = AF_INET;
+	ser_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	ser_addr.sin_port = htons(12345);
+	
+	ser_fd = Socket(AF_INET,SOCK_STREAM,0);
+	Bind(ser_fd,(struct sockaddr *) &ser_addr,sizeof(ser_addr));
+	Listen(ser_fd,1024);
+	
+	int flags = fcntl(ser_fd,F_GETFL,0);
+	fcntl(ser_fd,F_SETFL,flags | O_NONBLOCK);
+
+	if((ep_fd = epoll_create(1024)) < 0){
+		cout<<"error"<<endl;
+		return(-1);
+	}
+	//to be continue;
+
+	close(ser_fd);
+	return(0);
+}
