d46415101f1ffe6e3be373558415043aa746b1ad
qw3320400 <464822699@qq.com>
2014-08-02 19:17:54 +0800

    Update test_cli.cpp

--- test_cli.cpp
+++ test_cli.cpp
@@ -12,7 +12,8 @@ int main(int argc,char **argv){
 	bzero(&cli_add,sizeof(cli_add));
 	cli_add.sin_family = AF_INET;
 	cli_add.sin_port = htons(12345);
-	inet_pton(AF_INET,"121.10.118.102",&cli_add.sin_addr);
+	//IP
+	inet_pton(AF_INET,"IP",&cli_add.sin_addr);
 
 	cli_fd = socket(AF_INET,SOCK_STREAM,0);
 	connect(cli_fd,(struct sockaddr *) &cli_add,sizeof(cli_add));
